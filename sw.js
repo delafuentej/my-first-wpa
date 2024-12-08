2b21847e73bcbe1a73ace55f0a68510f9eee7433
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-12-08 13:33:51 +0100

    style.css file changes

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //imports
 importScripts("js/sw-utils.js");
 
-let CACHE_STATIC = "static-v2";
+let CACHE_STATIC = "static-v3";
 const CACHE_DYNAMIC = "dynamic-v1";
 const CACHE_IMMUTABLE = "immutable-v1";
 
